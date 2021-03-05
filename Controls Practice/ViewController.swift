40d69c105dc66adb5e233722e6891d469537b54c
Serega <borzunov_sm2mail.ru>
2021-03-05 17:50:21 +0300

    Implemented switches setting and getting

--- Controls Practice/ViewController.swift
+++ Controls Practice/ViewController.swift
@@ -22,25 +22,42 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        rotateSwitches()
         updateUI()
         // Do any additional setup after loading the view.
+    }
+    
+    func rotateSwitches(){
+        for `switch` in switches{
+            `switch`.layer.transform = CATransform3DMakeRotation(-.pi / 2, 0, 0, 1)
+            
+        }
+    }
+    
+    /// Updates number from the switches
+    func updateNumberFromSwitches(){
+      var number = 0
+        for `switch` in switches{
+            number += `switch`.isOn ? `switch`.tag : 0
+        }
+        self.number = UInt8(number % 256)
+    }
+    
+    /// Updates switches from number
+    func updateSwitches(){
+        for `switch` in switches{
+            `switch`.isOn =  Int(number) & `switch`.tag != 0
+        }
     }
     
     /// updates all outlets to number
     func updateUI(){
         button.setTitle("\(number)", for: [])
-        
-        
-        /// TODO: set switches to number
-        
-        
+        updateSwitches()
         slider.value = Float.init(number)
-        
         textField.text = "\(number)"
     }
 
-    
-    
     @IBAction func buttonPressed() {
         number = UInt8((Int(number) + 1) % 256)
         //number += 1
@@ -48,7 +65,9 @@ class ViewController: UIViewController {
     }
     
     @IBAction func switchToggled(_ sender: UISwitch) {
-        print(#line, #function, sender.tag)
+       // print(#line, #function, sender.tag)
+        updateNumberFromSwitches()
+    
     }
     
     @IBAction func sliderMoved() {
@@ -61,5 +80,5 @@ class ViewController: UIViewController {
         //print(#line, #function)
     }
     
-}
 
+}
